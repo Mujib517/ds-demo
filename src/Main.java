918036a22815d255abd58a8139abbdb44fbf4ad3
Mujib <mujib517@gmail.com>
2021-01-24 10:23:12 +0530

    BST problems

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,25 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{5, 3, 2, 4, 1, 7, 6, 8};
+//        int[] arr = new int[]{5, 3, 2, 4, 1, 7, 6, 8};
+//
+//
+//        TNode result = TreeProblems.buildBST(arr);
 
-        TNode result = TreeProblems.buildBST(arr);
+        TNode root = new TNode(10);
+        TNode l1 = new TNode(8);
+        TNode l2 = new TNode(5);
+        TNode l3 = new TNode(11);
+        TNode r1 = new TNode(20);
 
-//        TNode res = TreeProblems.insertRec(result, 30);
 
-        TreeProblems.postOrder(result);
+        l1.left = l2;
+        l1.right = l3;
+        root.left = l1;
+        root.right = r1;
 
-//        System.out.println(TreeProblems.findMax(result));
+
+        System.out.println(TreeProblems.isBST(root));
     }
 
 }
--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeProblems {
 
     // NLogN
@@ -121,5 +124,113 @@ public class TreeProblems {
         postOrder(root.left);
         postOrder(root.right);
         System.out.println(root.data);
+    }
+
+    // O(N). Binary Tree
+    public static TNode parent(TNode root, int x) {
+        if (root == null || root.data == x) return null;
+
+        if (isParent(root, x)) return root;
+
+        TNode result = parent(root.left, x);
+        if (result != null) return result;
+        return parent(root.right, x);
+    }
+
+    // LogN
+    public static TNode parentBST(TNode root, int x) {
+        if (root == null || root.data == x) return null;
+
+        if (isParent(root, x)) return root;
+        if (root.data < x) return parentBST(root.right, x);
+        return parentBST(root.left, x);
+    }
+
+    public static int height(TNode root) {
+        if (root == null) return 0;
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static boolean areSiblings(TNode root, int x, int y) {
+        if (root == null) return false;
+
+        if (areChildren(root, x, y)) return true;
+        return areSiblings(root.left, x, y) ||
+                areSiblings(root.right, x, y);
+    }
+
+    public static void levelOrder(TNode root) {
+        if (root == null) return;
+
+        Queue<TNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            TNode nd = q.poll();
+            System.out.println(nd.data);
+            if (nd.left != null) q.add(nd.left);
+            if (nd.right != null) q.add(nd.right);
+        }
+    }
+
+    // will not work for all BSTs
+    public static boolean isBST(TNode root) {
+        if (root == null) return true;
+        if (!isValid(root)) return false;
+
+        return isBST(root.left) && isBST(root.right);
+    }
+
+    public static void levelOrder2(TNode root) {
+        if (root == null) return;
+
+        Queue<TNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            TNode nd = q.poll();
+            if (nd != null) {
+                System.out.print(nd.data + " ");
+                if (nd.left != null) q.add(nd.left);
+                if (nd.right != null) q.add(nd.right);
+            } else if (nd == null && !q.isEmpty()) {
+                q.add(null);
+                System.out.println();
+            }
+        }
+
+    }
+
+    private static boolean areChildren(TNode root, int x, int y) {
+        boolean cond1 = root.left != null
+                && root.left.data == x
+                && root.right != null
+                && root.right.data == y;
+
+        boolean cond2 = root.left != null
+                && root.left.data == y
+                && root.right != null
+                && root.right.data == x;
+
+        return cond1 || cond2;
+    }
+
+    private static boolean isParent(TNode root, int x) {
+        return (root.left != null && root.left.data == x) ||
+                (root.right != null && root.right.data == x);
+    }
+
+    private static boolean isValid(TNode root) {
+        if (root.left != null && root.right != null) {
+            return root.data < root.right.data && root.data >= root.left.data;
+        } else if (root.left == null && root.right != null) {
+            return root.data < root.right.data;
+        } else if (root.left != null && root.right != null)
+            return root.data >= root.left.data;
+        return true;
     }
 }
